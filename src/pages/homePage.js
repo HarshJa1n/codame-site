aa67047aaca87f56152c3f679fece01b6cd713a9
HarshJa1n <hjain012003@gmail.com>
2022-09-27 13:53:38 +0530

    fixes

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import FirstSection from "../components/firstSection";
 import Header from "../components/common/header";
-
 import ThirdSection from "../components/thirdSection";
 import BlueSection from "../components/blueSection";
 import PurpleSection from "../components/purpleSection";
